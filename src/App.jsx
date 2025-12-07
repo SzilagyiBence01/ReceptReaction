364657d961b7d091a2e74487079b59c2e954688e
SzilagyiBence01 <144115416+SzilagyiBence01@users.noreply.github.com>
2025-12-07 17:56:30 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div>
+      <h1>Recept Reaction</h1>
+    </div>
+  );
+}
---
+++ src/components/RecipeForm.jsx
@@ -0,0 +1,12 @@
+import React,{useState} from "react"
+
+export default function RecipeForm(){
+
+    const [recipeTitle, setRecipeTitle] = useState(""); //useContext
+
+    return(
+        <div>
+
+        </div>
+    )
+}
